17ff76273676b03f7c76261f9197f52a02c19e73
singhtushar689 <singhtushar689@gmail.com>
2022-11-11 15:13:41 +0530

    Check Whether Length of two lines are equal or not

--- Program.cs
+++ Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Line Comparison");
+            Console.WriteLine("Enter the Co-ordinates of the Line");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Double Line1 = Math.Pow((x2 - x1), 2);
+            Double Line2 = Math.Pow((y2 - y1), 2);
+            Double TotalLength = Line1 + Line2;
+            TotalLength = Math.Sqrt(Line1 + Line2);
+            Console.WriteLine("The Total Length of the Line is :" + TotalLength);
+
+
+
         }
     }
 }
