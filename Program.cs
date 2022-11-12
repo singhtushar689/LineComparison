70ff94d7590098a0674b300d459a28be940f98a9
singhtushar689 <singhtushar689@gmail.com>
2022-11-12 12:26:38 +0530

    Comparison between Two lines

--- Program.cs
+++ Program.cs
@@ -6,30 +6,33 @@ namespace LineComparison
     {
         static void Main()
         {
-            Console.WriteLine("Welcome To the Line Comparison Program");
-            Console.WriteLine("Enter the Co-Ordinates of the Line1");
+            Console.WriteLine("Welcome to the Line Comparison Problems");
+            Console.WriteLine("Enter the Co-Ordinates of Line1");
             int x1 = Convert.ToInt32(Console.ReadLine());
-            int x2 = Convert.ToInt32(Console.ReadLine());   
-            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());   
             int y2 = Convert.ToInt32(Console.ReadLine());
-          
-            Double Points1 = Math.Pow((x2 - x1), 2);
-            Double Points2 = Math.Pow((y2 - y1), 2);
-            Double TotalLengthOfLine1 = Math.Sqrt(Points1 + Points2);
+            
+            Double Points1 = Math.Pow((x2-x1), 2);
+            Double Points2 = Math.Pow((y2-y1), 2);
+            Double LengthOfLine1 = Math.Sqrt(Points1+Points2);
 
-            Console.WriteLine("Enter the Co-Ordinates of the Line2");
+            Console.WriteLine("Enter the Co-Ordinates of Line2");
             int a1 = Convert.ToInt32(Console.ReadLine());
             int a2 = Convert.ToInt32(Console.ReadLine());
             int b1 = Convert.ToInt32(Console.ReadLine());
             int b2 = Convert.ToInt32(Console.ReadLine());
-            Double Points3 = Math.Pow((a2-a1),2);
-            Double Points4 = Math.Pow((b2-b1),2);   
-            Double TotalLengthOfLine2 = Math.Sqrt(Points3 + Points4);
-            bool status = TotalLengthOfLine1.Equals(TotalLengthOfLine2);
-            if (status) 
-            Console.WriteLine("Line1 is equal to Line2");
+            
+            Double Points3 = Math.Pow((a2 - a1), 2);
+            Double Points4 = Math.Pow((b2 - b1), 2);
+            Double LengthOfLine2 = Math.Sqrt(Points3 + Points4);
+
+            if (LengthOfLine1.CompareTo(LengthOfLine2) < 0)
+                Console.WriteLine("Line2 is greater than Line1");
+            else if (LengthOfLine1.CompareTo(LengthOfLine2) > 0)
+                Console.WriteLine("Line1 is greater than Line2");
             else
-            Console.WriteLine("Line1 is not equal to Line2");
+                Console.WriteLine("Line1 is equal to Line2");
         }
     }
 }
